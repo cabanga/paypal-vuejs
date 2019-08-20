a6fea69a285d86264ef51854807bb82b15bad010
Cabanga <manucabanga@gmail.com>
2019-08-20 14:29:05 +0100

    config paypal on project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,5 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import PayPal from 'vue-paypal-checkout'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-    PayPal
-  },
-  data() {
-    return {
-      paypal: {
-        sandbox: 'AXHOQJir4UbLH5hEUbJXJiZiWGV12rNfj_LRPTXbyHe3W7FZfWXKxqoQ3MJv1sY0dsUxi4y9aj7MGqSd',
-        production: 'EI7Ueh7U5Yq_H-EgHp62zyUOMNUcIS07Azav3_D-FG41Fq1MrxLlyygiQc5_DWSqomiua8-tAu4cw746'
-      }
-    }
-  }
-}
-</script>
